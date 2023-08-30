8efb07a93212204e0a864a4301025c92d874aa9e
Paul Cheng <chengyuejia@foxmail.com>
2023-08-30 20:53:12 +0800

    Add uni-sumcheck todo notes

--- 10_uni_sumcheck/src/lib.rs
+++ 10_uni_sumcheck/src/lib.rs
@@ -1,6 +1,4 @@
-//! prove that a specified univariate polynomial p of degree D sums to 0 over a multiplicative subgroup H with |H| = n,
 mod domain;
 mod poly;
-mod prover;
+mod sumcheck;
 mod transcript;
-mod verifier;
